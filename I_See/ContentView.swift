e17e95af6d3e2685c241a020ef2ad30e720ffb03
Marshall Buck <marshall@shootforthegoal.com>
2022-10-17 00:21:45 -0400

    added speech code

--- I_See/ContentView.swift
+++ I_See/ContentView.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var name : String = "find item 1"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            TextField("Name", text: $name)
+                .padding()
+    
+            Button("Greet") {
+                let utterance = AVSpeechUtterance(string: "Hello \(name)")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-au")
+                utterance.rate = 0.3
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.speak(utterance)
+            }
+            
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
