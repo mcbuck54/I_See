33b11b327ac634f7ceabb157a559761e028ebd36
Marshall Buck <marshall@shootforthegoal.com>
2022-10-16 23:50:47 -0400

    Initial Commit

---
+++ I_See/I_SeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_SeeApp.swift
+//  I_See
+//
+//  Created by MCBUCK54 on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct I_SeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ I_SeeTests/I_SeeTests.swift
@@ -0,0 +1,35 @@
+//
+//  I_SeeTests.swift
+//  I_SeeTests
+//
+//  Created by MCBUCK54 on 10/16/22.
+//
+
+import XCTest
+
+final class I_SeeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
